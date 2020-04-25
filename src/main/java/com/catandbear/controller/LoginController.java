9f88d0a3c7831b3d22ece14a9e4cfe21cbe51f48
Yi Zheng Cao <yzcaodl@cn.ibm.com>
2020-04-25 19:05:56 +0800

    comit

--- src/main/java/com/catandbear/controller/LoginController.java
+++ src/main/java/com/catandbear/controller/LoginController.java
@@ -1,14 +1,11 @@
 package com.catandbear.controller;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,50 +16,13 @@ import com.catandbear.vo.AuthUser;
 @RestController
 @CrossOrigin(origins="*")
 public class LoginController {
-
-	@GetMapping("login")
-	public AuthReturn authUnamePwd(@RequestBody(required=false) AuthUser authUser,
-			HttpServletResponse resp) {
-		
-		AuthReturn ar = new AuthReturn();
-		ar.setIsAuth(false);
-		ar.setToken("");
-		
-		if (authUser!=null) {
-			
-			System.out.println("input json: " + authUser.toString());
-			
-			String token = getRandomString(16);
-			System.out.println("token : " + token);
-			
-			if (authUser.getUsername()=="root") {
-				Cookie userCookie=new Cookie("ULOGIN","admin"); 
-				userCookie.setMaxAge(7*24*60*60);    //设置cookie的最大生命周期为一周
-				userCookie.setPath("/");    //设置路径为全路径（这样写的好处是同一项目下的页面都可以访问该cookie）
-				resp.addCookie(userCookie);   //response是HttpServletResponse类型
-				
-				ar.setIsAuth(true);
-				ar.setToken(token);
-				
-			} else if (authUser.getUsername()=="yhyancy") {
-				Cookie userCookie=new Cookie("ULOGIN","user"); 
-				userCookie.setMaxAge(7*24*60*60);    //设置cookie的最大生命周期为一周
-				userCookie.setPath("/");    //设置路径为全路径（这样写的好处是同一项目下的页面都可以访问该cookie）
-				resp.addCookie(userCookie);   //response是HttpServletResponse类型
-				ar.setIsAuth(true);
-				ar.setToken(token);
-			}
-		}
-		
-		
-		return ar;
-	}
 	
 	@PostMapping("login")
-	public AuthReturn authUnamePwdPost(@RequestBody(required=false) AuthUser authUser,
+	public AuthReturn authUnamePwd(@RequestBody(required=false) AuthUser authUser,
 			HttpServletResponse resp) {
 		AuthReturn ar = new AuthReturn();
-		Cookie userCookie;
+		Cookie userCookie = null;
+		String token = getRandomString(16);
 		ar.setIsAuth(false);
 		ar.setToken("");
 		
@@ -70,23 +30,23 @@ public class LoginController {
 			
 			System.out.println("input json: " + authUser.toString());
 			
-			String token = getRandomString(16);
+			
 			System.out.println("token : " + token);
 			
-			if (authUser.getUsername()=="root") {
+			if ("root".equals(authUser.getUsername())) {
 				userCookie=new Cookie("ULOGIN","admin"); 
-				userCookie.setMaxAge(7*24*60*60);    //设置cookie的最大生命周期为一周
-				userCookie.setPath("/");    //设置路径为全路径（这样写的好处是同一项目下的页面都可以访问该cookie）
-				resp.addCookie(userCookie);   //response是HttpServletResponse类型
+				userCookie.setMaxAge(7*24*60*60);
+				userCookie.setPath("/");   
+				resp.addCookie(userCookie); 
 				
 				ar.setIsAuth(true);
 				ar.setToken(token);
 				
-			} else if (authUser.getUsername()=="yhyancy") {
+			} else if ("yhyancy".equals(authUser.getUsername())) {
 				userCookie=new Cookie("ULOGIN","user"); 
-				userCookie.setMaxAge(7*24*60*60);    //设置cookie的最大生命周期为一周
-				userCookie.setPath("/");    //设置路径为全路径（这样写的好处是同一项目下的页面都可以访问该cookie）
-				resp.addCookie(userCookie);   //response是HttpServletResponse类型
+				userCookie.setMaxAge(7*24*60*60); 
+				userCookie.setPath("/");
+				resp.addCookie(userCookie);
 				ar.setIsAuth(true);
 				ar.setToken(token);
 			}
