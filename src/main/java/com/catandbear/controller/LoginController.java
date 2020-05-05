3ee9c4d44d171321d2758dd525f77586adb00b5d
Yi Zheng Cao <yzcaodl@cn.ibm.com>
2020-05-05 22:43:54 +0800

    ~

--- src/main/java/com/catandbear/controller/LoginController.java
+++ src/main/java/com/catandbear/controller/LoginController.java
@@ -6,7 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +23,7 @@ public class LoginController {
 	@Autowired
 	UserInfoMapper userMapper;
 
-	@GetMapping("login")
+	@PostMapping("login")
 	public LoginReturn authUnamePwd(@RequestBody(required = true) UserInfo authUser, HttpServletResponse resp,
 			HttpServletRequest req) {
 		// Parameter verification
