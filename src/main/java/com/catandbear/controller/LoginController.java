c7cabc9639c60df31647c8e301a3a1133b387f7a
Yi Zheng Cao <yzcaodl@cn.ibm.com>
2020-05-10 16:28:09 +0800

    commit login function

--- src/main/java/com/catandbear/controller/LoginController.java
+++ src/main/java/com/catandbear/controller/LoginController.java
@@ -1,9 +1,6 @@
 package com.catandbear.controller;
 
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
